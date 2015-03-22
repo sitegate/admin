d5c8dcbe257ec674ce0e7aa99eba72103dea71d1
Zoltan Kochan <ZoltanKochan@gmail.com>
2015-03-22 22:43:58 +0200

    Only users with the admin role can access the admin site.

--- app/controllers/auth.js
+++ app/controllers/auth.js
@@ -5,16 +5,13 @@ var passport = require('passport');
 exports.oauthCallback = function (strategy) {
   return function (req, res, next) {
     passport.authenticate(strategy, function (err, user, redirectURL) {
-      if (req.user) {
-        return res.redirect('/');
-      }
-      if (err || !user) {
-        return res.redirect('/');
+      if (err || !user || user.role !== 'admin') {
+        return res.render('access-denied');
       }
       
       req.login(user, function (err) {
         if (err) {
-          return res.redirect('/');
+          return res.render('access-denied');
         }
 
         return res.redirect('/');
